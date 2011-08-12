86dd90de8decad8c50cba8dc744de89331d89848
Joey Nenni <joey.nenni@gmail.com>
2011-08-12 13:37:07 -0700

    first commit

---
+++ examples/server.js
@@ -0,0 +1,5 @@
+// this line should be used if the example was in the same directory
+// as the 'node_modules' directory containing the 'milk' directory
+// require('milk').start();
+
+require('../lib/milk').start();
